32ce04a585e61627ae56eea53eb5d32e8f7ee570
Deepak Kumar <deepaksharma2004kotala@gmail.com>
2025-05-12 18:05:01 -0700

    first commit

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about container">
+      <h4>Events</h4>
+      <h2>ABOUT</h2>
+      <p>
+        Welcome to Plan'X,
+        your all-in-one solution for effortless event planning and seamless coordination. Our platform is designed to simplify the way individuals and organizations manage events — from small personal gatherings to large-scale corporate functions.
+
+        With a user-friendly interface and powerful tools, Plan'X helps you create, organize, and track every detail of your event. Whether it’s booking venues, sending invitations, managing RSVPs, or handling schedules, we bring everything together in one centralized space.
+
+        Our mission is to streamline event management through smart technology and intuitive design — saving you time, reducing stress, and ensuring every event is a success. 
+
+
+      </p>
+      
+      
+      <p>💼 Our Services: 1. Event Creation & Customization 2. User Dashboards 3. Real-time Notifications 4. Feedback & Rating System</p>
+    </section>
+  );
+};
+
+export default About;
